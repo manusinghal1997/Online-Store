95025065b5ba0bfe181aad00193215e486e7a9fc
DESKTOP-IODDM1V\Manu Singhal <manusinghal1997@gmail.com>
2020-09-24 12:31:25 +0530

    User added to database

--- projbackend/controllers/auth.js
+++ projbackend/controllers/auth.js
@@ -1,3 +1,24 @@
+const User = require("../modals/user");
+
+
+exports.signup = (req, res) => {
+    const user = new User(req.body);
+    user.save((err, user) => {
+      if (err) {
+          console.log(err);
+        return res.status(400).json({
+          err: "NOT able to save user in DB"
+        });
+      }
+      res.json({
+        name: user.name,
+        email: user.email,
+        id: user._id
+      });
+    });
+  };
+
+
 exports.signout = (req,res) =>{
     res.send("user signout from controller successfully");
 };
